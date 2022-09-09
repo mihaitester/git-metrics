c9908532245e29edbf69a603f3569df7dbc1b5e2
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-09-09 10:30:31 +0300

    fix(readme): more helper links

---
+++ git-metrics.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# git show <commit> --name-status
+# todo: figure out how to get the commits that are closest to input dates
+# git diff <commit1> <commit2> --name-status | grep "A" - get added files - new code
+# git diff <commit1> <commit2> --name-status | grep "M" - get modified files - maintenance
