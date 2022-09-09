60f040baa67fafece37aba1ad2b8aa2b80beba21
Mihai Moldovan <mihaiflorinmoldovan@yahoo.com>
2022-09-09 12:44:23 +0300

    fix(script): finally managed to get wanted results, needed `var=$()` instead of `var=()`

--- git-metrics.sh
+++ git-metrics.sh
@@ -7,30 +7,34 @@ else
 fi
 
 default_branch="master"
-
 start_date="${review_year}-01-01 00:00"
 end_date="${review_year}-12-31 23:59"
 
 # git show <commit> --name-status
 # todo: figure out how to get the commits that are closest to input dates
 
-# git log --reverse --after="2022-01-01" --before="2022-12-31" master \| grep commit
-beginning_commits=(git log --reverse --after="${start_date}" --before="${end_date}" ${default_branch} \| grep commit)
-# git log --after="2022-01-01" --before="2022-12-31" master \| grep commit
-ending_commits=(git log --after="${start_date}" --before="${end_date}" ${default_branch} \| grep commit)
+# git log -q --reverse --after="2022-01-01" --before="2022-12-31" master | grep commit | head -n 1 | cut -d' ' -f2-
+beginning_commits=$(git log -q --reverse --after="${start_date}" --before="${end_date}" ${default_branch} | grep commit)
+# git log -q --after="2022-01-01" --before="2022-12-31" master | grep commit | head -n 1 | cut -d' ' -f2-
+ending_commits=$(git log -q --after="${start_date}" --before="${end_date}" ${default_branch} | grep commit)
 
-first_commit=(echo "${beginning_commits}" \| head -n 1 \| cut -d' ' -f2-) # get first commit
-last_commit=(echo "${ending_commits}" \| head -n 1 \| cut -d' ' -f2-) # get last commit
+first_commit=$(echo "${beginning_commits}" | head -n 1 | cut -d' ' -f2-) # get first commit
+# first_commit=$(git log --q --reverse --after="${start_date}" --before="${end_date}" ${default_branch} | grep commit | head -n 1 | cut -d' ' -f2-) # get first commit
+last_commit=$(echo "${ending_commits}" | head -n 1 | cut -d' ' -f2-) # get last commit
+# last_commit=$(git log --after="${start_date}" --before="${end_date}" ${default_branch} | grep commit | head -n 1 | cut -d' ' -f2-) # get last commit
+
 echo "First commit of year: ${first_commit}"
+#git show ${first_commit} --name-only
 echo "Last commit of year: ${last_commit}"
+#git show ${last_commit} --name-only
 
-all_files=(git diff ${first_commit} ${last_commit} --name-only) # get all files
-new_files=(git diff ${first_commit} ${last_commit} --name-status \| grep "A") # get added files - new code
-maintained_files=(git diff ${first_commit} ${last_commit} --name-status \| grep "M") # get modified files - maintenance
+all_files=$(git diff ${first_commit} ${last_commit} --name-only) # get all files
+new_files=$(git diff ${first_commit} ${last_commit} --name-status | grep "A") # get added files - new code
+maintained_files=$(git diff ${first_commit} ${last_commit} --name-status | grep "M") # get modified files - maintenance
 
-count_all_files=(echo "${all_files}" \| wc -l)
-count_new_files=(echo "${new_files}" \| wc -l)
-count_maintained_files=(echo "${maintained_files}" \| wc -l)
+count_all_files=$(echo "${all_files}" | wc -l)
+count_new_files=$(echo "${new_files}" | wc -l)
+count_maintained_files=$(echo "${maintained_files}" | wc -l)
 
 echo "=========="
 echo "Year review: ${review_year}"
@@ -38,6 +42,6 @@ echo "All files: ${count_all_files}"
 if ((${count_all_files} > 0)); then
     echo "New files: ${count_new_files}"
     echo "Maintained files: ${count_maintained_files}"
-    echo "Maintenance percent: " $((${count_maintained_files} / ${count_all_files} * 100))"%"
+    echo "Maintenance percent: " $(( 100 * ${count_maintained_files} / ${count_all_files} ))"%"
 fi
 echo "=========="
